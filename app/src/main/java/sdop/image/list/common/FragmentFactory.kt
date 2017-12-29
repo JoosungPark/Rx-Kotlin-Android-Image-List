d3b5c53e1d2d7e5ccd3c96a0909171461d446b82
jei.park <jei.park@peeper.co>
2017-12-29 11:03:20 +0900

    fragment factory

---
+++ app/src/main/java/sdop/image/list/common/FragmentFactory.kt
@@ -0,0 +1,28 @@
+package sdop.image.list.common
+
+import sdop.image.list.BaseFragment
+import sdop.image.list.controller.HomeFragment
+import sdop.image.list.controller.ImageFragment
+
+/**
+ *
+ * Created by jei.park on 2017. 12. 26..
+ */
+data class FragmentBundle(val type: FragmentFactory.FragmentType, val arg: String)
+
+class FragmentFactory {
+
+    enum class FragmentType {
+        Home,
+        Images
+    }
+
+    companion object {
+        fun createFragment(bundle: FragmentBundle) = createFragment(bundle.type, bundle.arg)
+
+        fun createFragment(type: FragmentType, arg: String? = null): BaseFragment = when (type) {
+            FragmentType.Home -> HomeFragment.newInstance()
+            FragmentType.Images -> ImageFragment.newInstance()
+        }
+    }
+}
