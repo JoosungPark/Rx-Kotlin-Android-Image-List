04b286adde7f0a81b3e3764aa9f2ffc6d2d169d8
jei.park <jei.park@peeper.co>
2018-07-12 16:02:15 +0900

    simplify viewmodel.

--- app/src/main/java/sdop/image/list/rx/Variable.kt
+++ app/src/main/java/sdop/image/list/rx/Variable.kt
@@ -1,7 +1,10 @@
 package sdop.image.list.rx
 
 import android.databinding.ObservableField
+import android.os.Parcel
+import android.os.Parcelable
 import io.reactivex.subjects.BehaviorSubject
+import java.io.Serializable
 
 /**
  *
@@ -17,6 +20,106 @@ class Nullable<T>(_value: T?) {
         fun <T> from(value: T?): Nullable<T> {
             return Nullable(value)
         }
+    }
+}
+
+class VariableSerializable<T : Serializable>(private var _value: T) : ObservableField<T>(_value), Parcelable, Serializable {
+
+    private val serializedSubject = BehaviorSubject.createDefault(_value)
+
+    override fun get() = _value
+
+    override fun set(value: T) {
+        this._value = value
+        super.notifyChange()
+
+        serializedSubject.onNext(this._value)
+    }
+
+    var value: T
+        @Synchronized get() = this._value
+        @Synchronized set(value) {
+            this._value = value
+            super.notifyChange()
+            serializedSubject.onNext(this._value)
+        }
+
+    fun asObservable() = serializedSubject
+
+    fun rx(): (T) -> Unit {
+        return fun(value: T) {
+            this.set(value)
+        }
+    }
+
+    override fun notifyChange() {
+        super.notifyChange()
+        serializedSubject.onNext(this._value)
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeSerializable(_value)
+
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<VariableParcelable<*>> {
+        override fun createFromParcel(source: Parcel): VariableParcelable<*> {
+            return VariableParcelable(source.readParcelable<VariableParcelable<*>>(this::class.java.classLoader))
+        }
+
+        override fun newArray(size: Int): Array<VariableParcelable<*>?> = arrayOfNulls(size)
+    }
+}
+
+class VariableParcelable<T : Parcelable>(private var _value: T) : ObservableField<T>(_value), Parcelable, Serializable {
+
+    private val serializedSubject = BehaviorSubject.createDefault(_value)
+
+    override fun get() = _value
+
+    override fun set(value: T) {
+        this._value = value
+        super.notifyChange()
+
+        serializedSubject.onNext(this._value)
+    }
+
+    var value: T
+        @Synchronized get() = this._value
+        @Synchronized set(value) {
+            this._value = value
+            super.notifyChange()
+            serializedSubject.onNext(this._value)
+        }
+
+    fun asObservable() = serializedSubject
+
+    fun rx(): (T) -> Unit {
+        return fun(value: T) {
+            this.set(value)
+        }
+    }
+
+    override fun notifyChange() {
+        super.notifyChange()
+        serializedSubject.onNext(this._value)
+    }
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeParcelable(_value, flags)
+
+    }
+
+    override fun describeContents(): Int = 0
+
+    companion object CREATOR : Parcelable.Creator<VariableParcelable<*>> {
+        override fun createFromParcel(source: Parcel): VariableParcelable<*> {
+            return VariableParcelable(source.readParcelable<VariableParcelable<*>>(this::class.java.classLoader))
+        }
+
+        override fun newArray(size: Int): Array<VariableParcelable<*>?> = arrayOfNulls(size)
     }
 }
 
